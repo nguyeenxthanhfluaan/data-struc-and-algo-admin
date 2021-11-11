be6aa77c643047f6c547d1a7239e07886f48ec76
buivantan01 <buivantan1234566@gmail.com>
2021-11-11 15:27:32 +0700

    Create section client

--- api/config/db.js
+++ api/config/db.js
@@ -1,16 +1,16 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const mongoURI =
-	'mongodb+srv://user:algo_pass@cluster0.e7kbo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+  'mongodb+srv://user:algo_pass@cluster0.e7kbo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 const connectDb = async () => {
-	try {
-		await mongoose.connect(mongoURI, { useNewUrlParser: true })
-		console.log('mongodb connecting . . .')
-	} catch (err) {
-		console.log(err)
-		process.exit(1)
-	}
-}
+  try {
+    await mongoose.connect(mongoURI, { useNewUrlParser: true });
+    console.log('mongodb connecting . . .');
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
 
-module.exports = connectDb
+module.exports = connectDb;
