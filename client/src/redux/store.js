fbe0cf22188e8651dc3ace47a09603e8341d40df
buivantan01 <buivantan1234566@gmail.com>
2021-12-28 19:16:13 +0700

    Build back

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,16 +1,15 @@
-import { createStore, applyMiddleware, compose } from 'redux'
-import thunk from 'redux-thunk'
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './rootReducer';
 
-import rootReducer from './rootReducer'
-
-export const middleware = [thunk]
+export const middleware = [thunk];
 
 const composeEnhancers =
-	typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-		? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-				// Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
-		  })
-		: compose
-const enhancer = composeEnhancers(applyMiddleware(...middleware))
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
+const enhancer = composeEnhancers(applyMiddleware(...middleware));
 
-export const store = createStore(rootReducer, enhancer)
+export const store = createStore(rootReducer, enhancer);
